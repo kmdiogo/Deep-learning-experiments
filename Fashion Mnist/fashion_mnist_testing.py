91905cb6169c903e176e38a3e41b2e3c1cae52ba
kmdiogo <kmdiogo@student.ysu.edu>
2018-11-30 00:36:11 -0500

    - Initial repo commit

--- Fashion Mnist/fashion_mnist_testing.py
+++ Fashion Mnist/fashion_mnist_testing.py
@@ -3,46 +3,45 @@ from pathlib import Path
 from keras.preprocessing import image
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.datasets import fashion_mnist
 
 # These are the CIFAR10 class labels from the training data (in order from 0 to 9)
 class_labels = [
-    "Plane",
-    "Car",
-    "Bird",
-    "Cat",
-    "Deer",
-    "Dog",
-    "Frog",
-    "Horse",
-    "Boat",
-    "Truck"
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot"
 ]
 
 # Load the json file that contains the model's structure
-f = Path("model_structure.json")
+f = Path("fashion_mnist_structure.json")
 model_structure = f.read_text()
 
 # Recreate the Keras model object from the json data
 model = model_from_json(model_structure)
 
 # Re-load the model's trained weights
-model.load_weights("model_weights.h5")
+model.load_weights("fashion_mnist_weights.h5")
 
-path = '../Fashion Mnist/shoe.png'
-path = 'frog.png'
 # Load an image file to test, resizing it to 32x32 pixels (as required by this model)
-img = image.load_img(path, target_size=(32, 32))
-plt.imshow(img)
-plt.show()
+#img = image.load_img("shoe.png", target_size=(28, 28))
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+img = x_test[1]
 
 # Convert the image to a numpy array
-image_to_test = image.img_to_array(img)
-
+#image_to_test = image.img_to_array(img)
+#image_to_test = img.transpose(2,0,1).reshape(3,-1)
 # Add a fourth dimension to the image (since Keras expects a list of images, not a single image)
-list_of_images = np.expand_dims(image_to_test, axis=0)
+#list_of_images = np.expand_dims(image_to_test, axis=0)
 
 # Make a prediction using the model
-results = model.predict(list_of_images)
+results = model.predict(img)
 
 # Since we are only testing one image, we only need to check the first result
 single_result = results[0]
